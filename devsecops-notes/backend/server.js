88b9d3097eb46508ed643611546c9b7e15d68927
Shubham Khodape <skhodape@gmail.com>
2026-07-07 22:55:19 +0530

    feat: setup Express backend with initial API

---
+++ devsecops-notes/backend/server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        status: "success",
+        message: "DevSecOps Notes API is running 🚀"
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
